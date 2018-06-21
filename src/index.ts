f9f65950a4f7ec071d9f8451f7f8740aac9fa2bb
Markelle Kelly <markellekelly@gmail.com>
2018-06-20 17:19:19 -0700

    David's index

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,89 @@
 import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
+
 import {
-  ICommandPalette
-} from '@jupyterlab/apputils';
+  ICellTools
+} from '@jupyterlab/notebook';
+
+import {
+  CellTools
+} from '@jupyterlab/notebook';
+
+import {
+  Widget, PanelLayout
+} from '@phosphor/widgets'
+
+import {
+  Cell
+} from '@jupyterlab/cells'
+
+import {
+  Message
+} from '@phosphor/messaging'
 
 import '../style/index.css';
 
+const TAG_TOOL_CLASS = 'jp-cellTags-Tools';
+
+class TagsTool extends CellTools.Tool {
+
+  constructor(options: TagsTool.IOptions) {
+    super();
+    this.addClass(TAG_TOOL_CLASS);
+    let layout = this.layout = new PanelLayout();
+    let widget = new Widget();
+    widget.id = 'cellsTags-tool';
+    widget.title.label = 'Tags';
+    widget.title.closable = true;
+    let tabsBarTitle = document.createElement('div');
+    tabsBarTitle.innerHTML = 'Tags';
+    let addButton = document.createElement('button');
+    addButton.innerHTML = 'New Tag';
+    let _self = this;
+    addButton.onclick = function() {
+      console.log(_self.activeCell.model.metadata.get("tags"));
+    };
+    widget.node.appendChild(tabsBarTitle);
+    widget.node.appendChild(addButton);
+    layout.addWidget(widget);
+  }
+
+  /**
+   * Handle a change to the active cell.
+   */
+  protected onActiveCellChanged(msg: Message): void {
+    this.activeCell = this.parent.activeCell;
+  }
+
+  private activeCell: Cell = null;
+
+} 
+
+namespace TagsTool {
+  /**
+   * The options used to initialize a metadata editor tool.
+   */
+  export
+  interface IOptions {
+    /**
+     * The editor factory used by the tool.
+     */
+    activeCell: Cell
+  }
+}
 
 /**
- * Initialization data for the celltags extension.
+ * Initialization data for the jupyterlab-celltags extension.
  */
 const extension: JupyterLabPlugin<void> = {
-  id: 'celltags',
+  id: 'jupyterlab-celltags',
   autoStart: true,
-  activate: (app: JupyterLab) => {
-    console.log('JupyterLab extension celltags is activated!');
+  requires: [ICellTools], 
+  activate: (app: JupyterLab, cellTools: ICellTools) => {
+    let cell = cellTools.activeCell;
+    let tagsTool = new TagsTool({ activeCell: cell });
+    cellTools.addItem({tool: tagsTool})    
   }
 };
 
