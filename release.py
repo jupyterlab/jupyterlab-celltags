bf6002ac13c7eec0ac5ceb395aa5f750c12dd59c
telamonian <mklein@jhu.edu>
2019-10-08 20:14:28 -0400

    copied over updates to release.py

--- release.py
+++ release.py
@@ -1,25 +1,53 @@
 #!/usr/bin/env python
 
+import argparse as argp
 import json
 import subprocess
 
-## get single source of truth
-with open('package.json') as f:
-    info = json.load(f)
 
-version = info['version']
+def npmjs(dry=False):
+    """release on npmjs
+    """
+    # get single source of truth
+    with open('package.json') as f:
+        info = json.load(f)
+
+    version = info['version']
+
+    tag(version, 'npmjs', dry=dry)
+
+    if dry:
+        # dry run build and release
+        subprocess.run(['npm', 'publish', '--access', 'public', '--dry-run'])
+    else:
+        # build and release
+        subprocess.run(['npm', 'publish', '--access', 'public'])
 
 
-## git tagging
-tag = f"v{version}"
+def tag(version, kind, dry=False):
+    """git tagging
+    """
+    tag = f"{kind}_v{version}"
 
-subprocess.run(['git', 'tag', tag])
-subprocess.run(['git', 'push', 'origin', tag])
+    if dry:
+        print(f'would release with git tag: {tag}')
+    else:
+        subprocess.run(['git', 'tag', tag])
+        subprocess.run(['git', 'push', 'origin', tag])
 
 
-## npmjs stuff
-# dry run build and release
-# subprocess.run(['npm', 'publish', '--access', 'public', '--dry-run'])
+def main():
+    parser = argp.ArgumentParser()
 
-# build and release
-subprocess.run(['npm', 'publish', '--access', 'public'])
+    parser.add_argument('-d', '--dry', action='store_true')
+    parser.add_argument('--npmjs', action='store_true')
+
+    parsed = vars(parser.parse_args())
+    dry = 'dry' in parsed and parsed['dry']
+
+    if 'npmjs' in parsed and parsed['npmjs']:
+        npmjs(dry=dry)
+
+
+if __name__=='__main__':
+    main()
